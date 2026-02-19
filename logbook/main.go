5ba31f28b4c3d5a38f8d2fc50942edc34c994045
Allan Tabilog <allan.tabilog@pexa.com.au>
2026-02-19 15:39:37 +1100

    Refactor main and entries directory

--- logbook/main.go
+++ logbook/main.go
@@ -21,7 +21,7 @@ func saveEntry(entry Entry) error {
 	}
 
 	// Save to file
-	filename := fmt.Sprintf("entry_%d.json", entry.Timestamp.Unix())
+	filename := fmt.Sprintf("entries/entry_%d.json", entry.Timestamp.Unix())
 	err = os.WriteFile(filename, jsonData, 0644)
 	if err != nil {
 		return fmt.Errorf("error writing file: %w", err)
@@ -31,7 +31,19 @@ func saveEntry(entry Entry) error {
 	return nil
 }
 
-func addEntry(entry Entry) error {
+func addEntry(entry string) error {
+	newEntry := Entry{
+		Text:      entry,
+		Timestamp: time.Now(),
+	}
+
+	err := saveEntry(newEntry)
+	if err != nil {
+		fmt.Printf("Error: %v\n", err)
+		return err
+	}
+
+	fmt.Printf("New entry: %s created on %s\n", entry, newEntry.Timestamp.Format("Monday, January 2, 2006 at 3:04 PM"))
 	return nil
 }
 
@@ -45,35 +57,35 @@ func usage() {
 
 func main() {
 	fmt.Println("Welcome to Logbook!")
-	entry := flag.String("entry", "", "Create a new logbook entry")
-	flag.Parse()
-	fmt.Printf("Command line arguments: %v", flag.Args())
+
+	// Check if we have at least one argument (the subcommand)
+	if len(os.Args) < 2 {
+		usage()
+		return
+	}
+
+	// Get the subcommand
+	subcommand := os.Args[1]
 
 	// parse the commands
-	if flag.Arg(0) == "add" {
+	if subcommand == "add" {
+		// Create a new FlagSet for the 'add' subcommand
+		addCmd := flag.NewFlagSet("add", flag.ExitOnError)
+		entry := addCmd.String("entry", "", "Create a new logbook entry")
+
+		// Parse flags after the subcommand
+		addCmd.Parse(os.Args[2:])
+
+		if *entry == "" {
+			fmt.Println("To add a new entry please specify the entry via --entry option")
+			return
+		}
 		fmt.Println("Adding a new entry")
-	} else if flag.Arg(0) == "list" {
+		addEntry(*entry)
+	} else if subcommand == "list" {
 		fmt.Println("Listing all entrieds")
 	} else {
 		usage()
 		return
 	}
-
-	if *entry != "" {
-		newEntry := Entry{
-			Text:      *entry,
-			Timestamp: time.Now(),
-		}
-
-		err := saveEntry(newEntry)
-		if err != nil {
-			fmt.Printf("Error: %v\n", err)
-			return
-		}
-
-		fmt.Printf("New entry: %s created on %s\n", *entry, newEntry.Timestamp.Format("Monday, January 2, 2006 at 3:04 PM"))
-	} else {
-		fmt.Println("No new entry created.")
-	}
-
 }
